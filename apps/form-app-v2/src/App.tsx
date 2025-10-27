3756742e7e92698f9d64e46fa0ddb593085ec441
natheeshkumar.rangasamy <natheeshece@gmail.com>
2025-10-27 09:27:05 +0530

    Revert "feat: update pnpm-lock.yaml with new dependencies and versions; add start-form-builder-v2.sh script for development setup"
    This reverts commit b293729dedcf6f1e65bcfe03227f5b105e6cf2ce.

--- apps/form-app-v2/src/App.tsx
+++ apps/form-app-v2/src/App.tsx
@@ -3,7 +3,6 @@ import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
 import { Dashboard } from './pages/Dashboard';
 import { FormDashboard } from './pages/FormDashboard';
-import CollaborativeFormBuilder from './pages/CollaborativeFormBuilder';
 import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
@@ -35,14 +34,6 @@ function App() {
         element={
           <ProtectedRoute>
             <FormDashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/dashboard/form/:formId/collaborate/:tab"
-        element={
-          <ProtectedRoute>
-            <CollaborativeFormBuilder />
           </ProtectedRoute>
         }
       />
