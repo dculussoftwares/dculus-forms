9edb8b55734d5c970856b24f0ad1def3d4b9ab2a
natheeshkumar.rangasamy <natheeshece@gmail.com>
2026-02-07 09:59:19 +0530

    chore: Remove MongoDB Atlas Terraform infrastructure and associated deployment and destroy steps from multi-cloud workflows.

--- infrastructure/multi-cloud/terraform/mongodb/environments/production/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "dculus-global-terraform-assets-resource-grp"
-    storage_account_name = "dculusterraformstates"
-    container_name       = "dculus-forms-mongodb-production-state"
-    key                  = "terraform.tfstate"
-  }
-}
